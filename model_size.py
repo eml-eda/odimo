adcd410f74a39e599fccd03255679f41cfe96126
matteorisso <matteorisso96@gmail.com>
2022-07-06 15:05:47 +0200

    Bn folding

--- model_size.py
+++ model_size.py
@@ -15,7 +15,7 @@ print(args)
 # Build and Load pretrained model if specified
 if args.pretrained_model is not None:
     model = models.__dict__[args.arch](
-        args.pretrained_model, num_classes=args.num_classes, 
+        args.pretrained_model, num_classes=args.num_classes,
         fine_tune=False, analog_speedup=args.analog_speedup)
 else:
     model = models.__dict__[args.arch](
